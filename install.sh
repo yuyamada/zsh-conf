037f4b37885e6d033f6974b12d90acb7f4523814
Yu Yamada <yu.yamada@nex.nikkei.com>
2024-09-27 13:20:18 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ln -s ${HOME}/.zprezto/runcoms/myzpreztorc ${HOME}/.zpreztorc
 ln -s ${PWD}/zsh ${HOME}/.zsh
 ln -s ${PWD}/zshrc ${HOME}/.zshrc
-ln -s ${PWD}/zshrc ${HOME}/.config/sheldon/plugins.toml
+ln -s ${PWD}/plugins.toml ${HOME}/.config/sheldon/plugins.toml
 
 # restart zsh
 exec $SHELL -l
